dc78c43ad300c6b1213d596c7e7e590496189f19
Lina Budkova <linabudkova35@gmail.com>
2023-02-20 15:30:29 +0200

    add task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,40 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Layout } from '../layout/Layout/Layout';
-import { routes } from '../routes';
-import { CocktailDetail, Cocktails, Home } from '../views';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "../layout/Layout/Layout";
+import { routes } from "../routes";
+import { CocktailDetail, Cocktails, Home } from "../views";
+// import { Header } from "../layout/Header/Header";
+// import { Footer } from "../layout/Footer/Footer";
+// import styles from "../layout/Layout/Layout.module.css";
 
 export const App = () => {
   return (
-    <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
-      Home Page
-    </h1>
+    <Routes>
+      <Route path={routes.HOME} element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path={routes.COCKTAILS} element={<Cocktails />} />
+        <Route path={routes.COCKTAIL_ID} element={<CocktailDetail />} />
+      </Route>
+      <Route path="*" element={<Navigate to={routes.HOME} />} />
+    </Routes>
   );
 };
+
+// export const App = () => {
+//   return (
+//     <div className={styles.layout}>
+//       <Header />
+
+//       <main className={styles.main}>
+//         <Routes>
+//           <Route path={routes.HOME} element={<Home />} />
+//           <Route path={"/" + routes.COCKTAILS} element={<Cocktails />} />
+//           <Route path={"/" + routes.COCKTAIL_ID} element={<CocktailDetail />} />
+
+//           <Route path="*" element={<Navigate to="/" />} />
+//         </Routes>
+//       </main>
+
+//       <Footer />
+//     </div>
+//   );
+// };
